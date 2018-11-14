39c2ee98d578a646bb52b2fd90e39703808cd325
htenkanen <henrikki.tenkanen@helsinki.fi>
2018-11-14 16:06:10 +0200

    Add lesson codes

---
+++ test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# PRESSING F9 exectutes a line or a SELECTION in IPython console
+1+2
+print("Hello world")
+
+import numpy as np
+# See documentation with CNTRL + i
+np.linspace(0,1, 20)
+var1 = "Boo"
